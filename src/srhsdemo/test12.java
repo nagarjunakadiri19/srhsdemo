55046309da1240c29ce37b66893c1eb8d18d1866
91939 <91939@KKK-HOME>
2021-10-07 00:58:33 +0530

    test project

---
+++ src/srhsdemo/test12.java
@@ -0,0 +1,12 @@
+package srhsdemo;
+
+public class test12 {
+
+	public static void main(String[] args) {
+		System.out.println("hii....");
+		System.out.println("Git Practice by lead");
+		
+
+	}
+
+}
