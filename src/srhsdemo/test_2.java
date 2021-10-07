6dffde7ba69c7ae457f72aa234056cf09c146bf7
USER <USER@DESKTOP-J61QOPV>
2021-10-07 14:59:32 +0530

    new branch code commit

---
+++ src/srhsdemo/test_2.java
@@ -0,0 +1,12 @@
+package srhsdemo;
+
+public class test_2 {
+
+	public static void main(String[] args) {
+		
+		 System.out.println("Hiii");
+		 
+		 System.out.println("New test methode");
+	}
+
+}
