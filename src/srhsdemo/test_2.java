df4d8786c14243bf451e809f62ebba70514a16d6
USER <USER@DESKTOP-J61QOPV>
2021-10-07 16:19:10 +0530

    updated code

--- src/srhsdemo/test_2.java
+++ src/srhsdemo/test_2.java
@@ -4,9 +4,8 @@ public class test_2 {
 
 	public static void main(String[] args) {
 		
-		 System.out.println("Hiii");
+		 System.out.println("Hiii123");
 		 
-		 System.out.println("New test methode");
-	}
+		 	}
 
 }
